db084cb96e9730c6ec3db9255b26a9d60cbd0ace
esobirjon2197 <esobirjon2197@gmail.com>
2025-12-05 15:08:22 +0500

    Add pattern printing examples in main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# 1-m
+for i in range(0,3):
+    print("$ " * 5)
+
+
+# 2-m
+for i in range(1,5):
+    print("0 " * i)
+
+
+
+# 3-m
+for i in range(2, 5):
+    print(f"@ " * i)
+
+for i in range(3,1,-1):
+    print("@ " * i)
+
+
+# 4-m
+for i in range(1, 5):
+    print(f"{i} " * i, "#")
+
+
